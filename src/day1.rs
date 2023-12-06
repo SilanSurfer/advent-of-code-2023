799d95ce99118cb064e010735b291be17a16e802
SilanSurfer <szwarc.adam@gmail.com>
2023-12-06 11:19:35 +0000

    test: Added UTs for Day1

--- src/day1.rs
+++ src/day1.rs
@@ -2,7 +2,7 @@ pub fn read_input() -> Vec<&'static str> {
     include_str!("day1_input1.txt").lines().collect()
 }
 
-pub fn solution(input: Vec<&str>) -> u32 {
+pub fn solution_one(input: Vec<&str>) -> u32 {
     let mut sum = 0;
     for line in input {
         let digits: Vec<u32> = line
@@ -13,4 +13,26 @@ pub fn solution(input: Vec<&str>) -> u32 {
         sum += 10*digits[0] + digits[digits.len()-1];
     }
     return sum;
+}
+
+pub fn solution_two(input: Vec<&str>) -> u32 {
+    // zero one two three four five seven eight nine
+    return 142;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn first_part() {
+        let input = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+        assert_eq!(solution_one(input), 142);
+    }
+
+    #[test]
+    fn second_part() {
+        let input = vec!["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"];
+        assert_eq!(solution_two(input), 281);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod day1;
 
 fn main() {
     let input = day1::read_input();
-    println!("Output: {}", day1::solution(input));
+    println!("Output: {}", day1::solution_one(input));
 }
